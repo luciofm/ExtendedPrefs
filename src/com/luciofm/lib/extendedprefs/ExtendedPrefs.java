8c2cb62cfab33ba8c1bf18f98426fa7b708801ab
Lucio Maciel <luciofm@gmail.com>
2012-08-22 10:09:36 -0300

    Added licensing info to files.
    Signed-off-by: Lucio Maciel <luciofm@gmail.com>

--- src/com/luciofm/lib/extendedprefs/ExtendedPrefs.java
+++ src/com/luciofm/lib/extendedprefs/ExtendedPrefs.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011,2012 Lucio Maciel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.luciofm.lib.extendedprefs;
 
 import java.lang.reflect.Type;
@@ -31,6 +47,10 @@ public class ExtendedPrefs {
 
 	public static ExtendedPrefs getExtendedPreferences(Context context, String name, int mode) {
 		return new ExtendedPrefs(context, name, mode);
+	}
+
+	public static ExtendedPrefsEditor edit(Context context, String name) {
+		return getExtendedPreferences(context, name).edit();
 	}
 
 	private ExtendedPrefs(Context context, String name, int mode) {
